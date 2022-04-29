495bfa8aeabc0082f3e9dcdeff58b0543df68b8a
Borohard <43915070+Borohard@users.noreply.github.com>
2022-04-29 17:11:56 +0500

    Добавлены страницы Регистрации и Лидерборда

---
+++ ui/src/LeaderboardPage.vue
@@ -0,0 +1,214 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Skills</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<template>  
+    <div class="section">
+        <div class="teamImage">
+            <img src="@/assets/crown.png" class="ms-auto" width="120" height="120"/>
+        </div>
+        <div class="skills">Результаты</div>
+        
+        <div class="diagram" id="teams">
+            <div class="skillBLock">
+                <div class="teamImage">
+                    <img src="@/assets/team_logo_1.png" class="ms-auto" />
+                </div>
+                <div class="column">
+                    <span>25</span>
+
+                </div>
+            </div>
+            <div class="skillBLock">
+                <div class="teamImage">
+                    <img src="@/assets/team_logo_2.png" class="ms-auto" />
+                </div>
+                <div class="column">
+                    <span>30</span>
+                </div>
+            </div>
+            <div class="skillBLock">
+                <div class="teamImage">
+                    <img src="@/assets/team_logo_3.png" class="ms-auto" />
+                </div>
+                <div class="column">
+                    <span>40</span>
+                </div>
+            </div>
+            <div class="skillBLock">
+                <div class="teamImage">
+                    <img src="@/assets/team_logo_4.png" class="ms-auto" />
+                </div>
+                <div class="column">
+                    <span>20</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+   
+    
+
+    
+</script>
+
+<style>
+    .teamImage {
+        display: block;
+        padding-bottom: 20px;
+        
+    }
+    .section {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        width: 25%;
+    }
+
+    .skills {
+        font-weight: bold;
+        text-transform: uppercase;
+        color: black;
+        font-size: 50px;
+        margin-bottom: 30px;
+    }
+
+    .diagram {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        width: 100%;
+    }
+
+    .skillBLock {
+        height: 300px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .column {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 235px;
+    }
+
+    .skillBLock:first-child .column {
+        background: #d45dff;
+        height: calc(350px / 100 * 25); 
+        animation: appFir 2s 1;
+    }
+
+    .skillBLock:nth-child(2) .column {
+        background: #4cff00;
+        height: calc(350px / 100 * 30); 
+        animation: appSec 2s 1;
+    }
+
+    .skillBLock:nth-child(3) .column {
+        background: #f20c6c;
+        height: calc(350px / 100 * 40); 
+        animation: appThi 2s 1;
+    }
+
+    .skillBLock:last-child .column {
+        background: #b50cf2;
+        height: calc(350px / 100 * 20); 
+        animation: appFou 2s 1;
+    }
+
+    .column span {
+        
+        color: #fff;
+        font-weight: bold;
+        font-size: 20px;
+        animation: app 3s 1;
+    }
+
+    .name {
+        text-transform: uppercase;
+        color: black;
+        font-weight: bold;
+        font-size: 25px;
+        margin-top: 10px;
+    }
+
+    @keyframes appFir {
+        0% {
+            height: 0;
+        }
+
+        20% {
+            height: 0;
+        }
+
+        100% {
+            height: calc(350px / 100 * 25);
+        }
+    }
+
+    @keyframes appSec {
+        0% {
+            height: 0;
+        }
+
+        20% {
+            height: 0;
+        }
+
+        100% {
+            height: calc(350px / 100 * 30);
+        }
+    }
+
+    @keyframes appThi {
+        0% {
+            height: 0;
+        }
+
+        20% {
+            height: 0;
+        }
+
+        100% {
+            height: calc(350px / 100 * 40);
+        }
+    }
+
+    @keyframes appFou {
+        0% {
+            height: 0;
+        }
+
+        20% {
+            height: 0;
+        }
+
+        100% {
+            height: calc(350px / 100 * 20);
+        }
+    }
+
+    @keyframes app {
+        0% {
+            opacity: 0;
+        }
+
+        70% {
+            opacity: 0;
+        }
+
+        100% {
+            opacity: 1;
+        }
+    }
+</style>
---
+++ ui/src/RegisterPage.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="limiter">
+      <div class="container-login100">
+          <div class="wrap-login100 col-sm-3 mx-auto">             
+                  <div class="login100-form-title">
+                      <h1>СберКвест</h1>
+                  </div>
+
+                  <div class="login100-">
+                      <div class="form-group">
+                          <label for="name">ФИО</label>
+                          <input type="text" class="form-control" id="name" placeholder="Введите имя">
+
+                      </div>
+
+                      <div class="form-group">
+                          <label for="login">Логин</label>
+                          <input type="email" class="form-control" id="login" placeholder="Введите логин">
+
+                      </div>
+
+                      <div class="form-group">
+                          <label for="password">Пароль</label>
+                          <input type="password" class="form-control" id="password" placeholder="Введите пароль">
+                      </div>
+
+                      <div class="form-group">
+                          <label for="repeatPassword">Повторите пароль</label>
+                          <input type="password" class="form-control" id="repeatPassword" placeholder="Введите пароль">
+                      </div>
+
+
+                      <div class="form-group">
+                          <button type="submit" class="login_button">Зарегистрироваться</button>
+                      </div>
+
+                      <div class="form-group">
+                          <button type="submit" class="alreadyLogin_button">У меня уже есть аккаунт</button>
+                      </div>
+                  </div>                             
+          </div>      
+      </div>  
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  
+}
+</script>
+
+<style>
+    .login100-form {
+        width: 100%;
+    }
+
+    .login100-form-title {
+        display: block;
+        font-size: 30px;
+        color: #333333;
+        line-height: 1.2;
+        text-align: center;
+        padding: 10px 20px;
+    }
+    .login_button {
+        margin: 0 auto;
+        display: block;
+        background: rgb(0 211 27);
+        border: 0;
+        padding: 10px 20px;
+        color: white;
+        border-radius: 20px;
+        outline: 1px solid groove;
+    }
+    
+
+    .alreadyLogin_button {
+        margin: 0 auto;
+        display: block;
+        background: white;
+        border-color: rgb(0 211 27);
+        border: 1px solid rgb(0 211 27);
+        padding: 10px 20px;
+        color: rgb(4 182 7);
+        border-radius: 20px;
+    }
+
+    .wrap-login100 {
+        width: 300px;
+        background: #fff;
+        border-radius: 10px;
+        overflow: hidden;
+        padding: 55px 55px 33px 55px;
+        box-shadow: 0 5px 10px 0px rgba(0, 0, 0, 0.1);
+        -moz-box-shadow: 0 5px 10px 0px rgba(0, 0, 0, 0.1);
+        -webkit-box-shadow: 0 5px 10px 0px rgba(0, 0, 0, 0.1);
+        -o-box-shadow: 0 5px 10px 0px rgba(0, 0, 0, 0.1);
+        -ms-box-shadow: 0 5px 10px 0px rgba(0, 0, 0, 0.1);
+    }
+    .container-login100 {
+        width: 100%;
+        min-height: 100vh;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -moz-box;
+        display: -ms-flexbox;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        padding: 15px;
+        background: #f2f2f2;
+    }
+
+    .limiter {
+        width: 100%;
+        margin: 0 auto;
+    }
+
+</style>

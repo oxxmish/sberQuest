e0ff0796368c4d3057a6de8437be657efa1ec047
Michail <misha18031999@gmail.com>
2022-05-05 23:52:51 +0500

    Добавлен CORS фильтр

---
+++ backend/demo/src/main/kotlin/com/sber/quest/config/CorsConfig.kt
@@ -0,0 +1,24 @@
+package com.sber.quest.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+import java.util.*
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.allowedOrigins = Collections.singletonList("*")
+        config.allowedHeaders = Arrays.asList("Origin", "Content-Type", "Accept")
+        config.allowedMethods = Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+}
